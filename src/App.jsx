8c5f35a0584b9d2fdc64c139da0b6c7149db6f8e
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-11-20 10:40:00 +0200

    add future flags for ignoring the routing warnings in console

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import { router } from "./routes/appRoutes";
 import "./App.css";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  );
 }
 
 export default App;
